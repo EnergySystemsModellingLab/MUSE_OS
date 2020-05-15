b2b97082b7998bc1b18d5f8f1f89d4db5aab35b1
Giarola <sg2410@ic.ac.uk>
2020-05-15 16:14:23 +0100

    Added tests to aggregartors

--- tests/test_aggregoutput.py
+++ tests/test_aggregoutput.py
@@ -1,56 +1,62 @@
+import numpy as np
 import pandas as pd
-from xarray import DataArray
+from xarray import DataArray, concat
 from muse.utilities import reduce_assets
-from muse.sectors import AbstractSector
 from muse import examples
-from muse.outputs import save_output, to_csv
+from muse.outputs.sector import aggregate_sector, aggregate_sectors
 from typing import List
 
 
-def mca(loaded_residential_settings):
-    """Initialized MCA with the default settings and the residential sector."""
-    from muse.mca import MCA
+def test_aggregate_sector():
+    """Test for aggregate_sector function
+    check colum titles, number of agents/region/technologies
+    and assets capacities."""
+    mca = examples.model("multiple-agents")
+    sector_list = [sector for sector in mca.sectors if "preset" not in sector.name]
+    capa = aggregate_sector (sector_list[0], 2020)
+    assert "region" in capa.coords
+    assert "agent" in capa.coords
+    assert "sector" in capa.coords
+    assert "year" in capa.coords
+    assert "technology" in capa.coords
+    agent_names = [[a.name for a in sector.agents] for sector in sector_list
+    if "residential" in sector.name]
+    region_names = [[a.region for a in sector.agents] for sector in sector_list
+    if "residential" in sector.name]
+    technology_names = [[a.assets.technology.values[0] for a in sector.agents] for sector in sector_list
+    if "residential" in sector.name]
 
-    result = MCA.factory(loaded_residential_settings)
-
-    return result
-
-
-def aggregate_sector(sector: AbstractSector, year,
-                     columns=None) -> pd.DataFrame:
-    """Sector output to desired dimensions using reduce_assets"""
-    capa_sector = []
-    for u in sector.agents:
-        capa_agent = u.assets.capacity.sel(year=year)
-        capa_agent['agent'] = u.name
-        capa_agent['sector'] = sector.name
-        capa_sector.append(capa_agent)
-
-    capa_reduced = reduce_assets(capa_sector, coords=(["agent","sector","region","year","technology"]))
-    capa_reduced = capa_reduced.to_dataframe()#columns should be read here
-    return capa_reduced
-
-
-def aggregate_sectors(sectors: List[AbstractSector], year) -> pd.DataFrame:
-    """Aggregate outputs from all sectors"""
-    alldata = [aggregate_sector(sector, year)  for sector in sectors]
-    return pd.concat(alldata)
-
-
-path = """C:/Users/sg2410/Desktop/GitHub/StarMuse/src/muse/data/model/settings.toml"""
-
-
-market = mca(path)
-
-#
-# market.run()
-# for t in market.market.year:
-sector_list = [sector for sector in market.sectors if "preset" not in sector.name]
-
-capa = aggregate_sectors (sector_list, market.market.year[0])#year to be changed
+    expected_agent_names = list( [y for x in agent_names for y in x] )
+    expected_region_names = list( set([y for x in region_names for y in x] ) )
+    expected_technology_names = list(set( [y for x in technology_names for y in x]  ) )
+    obtained_technology_names = list( set( list(capa.technology.values)) ) 
+    expected_capacity= np.zeros(len(expected_agent_names))
+    for ui,u in enumerate(sector_list[0].agents):
+        expected_capacity[ui] = u.assets.capacity.sel(year=2020).sum(dim="asset")
+    assert sorted(list(capa.agent.values)) == sorted(expected_agent_names)
+    assert sorted([str(capa.region.values)]) == sorted(expected_region_names)
+    assert sorted(obtained_technology_names) == sorted(expected_technology_names)
+    assert expected_capacity.all() == capa.values.all()
 
 
-# def aggregate_sectors_tocsv(Dataframe, **kwargs) -> pd.Dataframe:
-#     """Returns output to csv"""
+def test_aggregate_sectors() -> DataArray:
+    """Test for aggregate_sectors function"""
+    mca = examples.model("multiple-agents")
+    sector_list = [sector for sector in mca.sectors if "preset" not in sector.name]
+    alldata = aggregate_sectors(sector_list, 2020)
+    agent_names = [[a.name for a in sector.agents] for sector in sector_list]
+    region_names = [[a.region for a in sector.agents] for sector in sector_list]
+    technology_names = [[a.assets.technology.values[0] for a in sector.agents] for sector in sector_list]
 
-#     return Dataframe.to_csv
+    expected_agent_names = list( [y for x in agent_names for y in x] )
+    expected_region_names = list( set([y for x in region_names for y in x] ) )
+    expected_technology_names = list(set( [y for x in technology_names for y in x]  ) )
+    obtained_technology_names = list( set( list(alldata.technology.values)) ) 
+    expected_capacity= np.zeros(len(expected_agent_names))
+    for ui,u in enumerate(sector_list[0].agents):
+        expected_capacity[ui] = u.assets.capacity.sel(year=2020).sum(dim="asset")
+    assert sorted(list(alldata.agent.values)) == sorted(expected_agent_names)
+    assert sorted([str(alldata.region.values)]) == sorted(expected_region_names)
+    assert sorted(obtained_technology_names) == sorted(expected_technology_names)
+    assert expected_capacity.all() == alldata.values.all()
+
