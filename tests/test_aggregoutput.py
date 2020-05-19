02d3e3e991db77576a5687e90157f97a67f65177
Giarola <sg2410@ic.ac.uk>
2020-05-19 11:31:54 +0100

    Updated tests removing nested loopes when possible

--- tests/test_aggregoutput.py
+++ tests/test_aggregoutput.py
@@ -8,6 +8,8 @@ def test_aggregate_sector():
     """Test for aggregate_sector function
     check colum titles, number of agents/region/technologies
     and assets capacities."""
+    from operator import attrgetter
+
     mca = examples.model("multiple-agents")
     sector_list = [sector for sector in mca.sectors if "residential" == sector.name]
     capa = aggregate_sector(sector_list[0], 2020)
@@ -16,20 +18,17 @@ def test_aggregate_sector():
     assert "sector" in capa.coords
     assert "year" in capa.coords
     assert "technology" in capa.coords
-    agent_names = [a.name for sector in sector_list for a in sector.agents]
-    region_names = [a.region for sector in sector_list for a in sector.agents]
-    technology_names = [
-        a.assets.technology.values[0] for sector in sector_list for a in sector.agents
-    ]
+    agent_names = [a.name for a in sector_list[0].agents]
+    region_names = [a.region for a in sector_list[0].agents]
+    technology_names = [a.assets.technology.values[0] for a in sector_list[0].agents]
 
     expected_capacity = [
         u.assets.capacity.sel(year=2020).sum(dim="asset").values
-        for sector in sector_list
-        for u in sector.agents
+        for u in sorted(sector_list[0].agents, key=attrgetter("name"))
     ]
     assert sorted(capa.agent.values) == sorted(agent_names)
-    assert sorted([str(region) for region in capa.region.values[None]]) == sorted(
-        np.unique(region_names)
+    assert sorted([str(capa.region.values) for _ in capa.agent.values]) == sorted(
+        region_names
     )
     assert sorted(capa.technology.values) == sorted(technology_names)
     assert (expected_capacity == capa.values).all()
@@ -37,6 +36,8 @@ def test_aggregate_sector():
 
 def test_aggregate_sectors() -> DataArray:
     """Test for aggregate_sectors function"""
+    from operator import attrgetter
+
     mca = examples.model("multiple-agents")
     sector_list = [sector for sector in mca.sectors if "preset" not in sector.name]
     alldata = aggregate_sectors(sector_list, 2020)
@@ -52,8 +53,8 @@ def test_aggregate_sectors() -> DataArray:
         for u in sector.agents
     ]
     assert sorted(alldata.agent.values) == sorted(agent_names)
-    assert sorted([str(region) for region in alldata.region.values[None]]) == sorted(
-        np.unique(region_names)
+    assert sorted([str(alldata.region.values) for _ in alldata.agent.values]) == sorted(
+        region_names
     )
     assert sorted(alldata.technology.values) == sorted(technology_names)
     assert (expected_capacity == alldata.values).all()
