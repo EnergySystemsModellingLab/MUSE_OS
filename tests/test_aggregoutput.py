48cc7ca4432a0877b4b0b326c006de9339447fbd
Giarola <sg2410@ic.ac.uk>
2020-06-02 22:43:13 +0100

    Flake error in test_aggregoutput

--- tests/test_aggregoutput.py
+++ tests/test_aggregoutput.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from muse import examples
 from muse.outputs.mca import sector_capacity, sectors_capacity
 
@@ -13,29 +11,27 @@ def test_aggregate_sector():
     year = [2020, 2025]
     sector_list = [sector for sector in mca.sectors if "preset" not in sector.name]
     agent_list = [a.agents for a in sector_list]
-    alldata = sectors_capacity(mca.sectors)
+    alldata = sector_capacity(sector_list[0])
     alldatadict = alldata.to_dict("split")
     columns = ["region", "agent", "type", "sector", "capacity"]
     assert (sorted(columns)) == sorted(alldatadict["columns"])
     frame = DataFrame()
-    for a, ai in enumerate(agent_list):
-        for ii in range(0, len(ai)):
-            for y in year:
-                if y in ai[ii].assets.year:
-                    if ai[ii].assets.capacity.sel(year=y).values > 0.0:
-                        data = DataFrame(
-                            {
-                                "region": ai[ii].region,
-                                "agent": ai[ii].name,
-                                "type": ai[ii].category,
-                                "sector": sector_list[a].name,
-                                "capacity": ai[ii]
-                                .assets.capacity.sel(year=y)
-                                .values[0],
-                            },
-                            index=[(y, ai[ii].assets.technology.values[0],)],
-                        )
-                        frame = concat([frame, data])
+    for ai in agent_list[0]:
+
+        for y in year:
+            if y in ai.assets.year:
+                if ai.assets.capacity.sel(year=y).values > 0.0:
+                    data = DataFrame(
+                        {
+                            "region": ai.region,
+                            "agent": ai.name,
+                            "type": ai.category,
+                            "sector": sector_list[0].name,
+                            "capacity": ai.assets.capacity.sel(year=y).values[0],
+                        },
+                        index=[(y, ai.assets.technology.values[0],)],
+                    )
+                    frame = concat([frame, data])
 
     assert (frame[columns].values == alldata[columns].values).all()
 
