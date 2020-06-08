276041498334ba08b020a71b0452fae0c31b0949
Adrian D'Alessandro <adr.dalessandro@gmail.com>
2020-06-08 16:21:09 +0100

    rename quantity to avoid name clash

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -299,7 +299,11 @@ def test_path_formatting(tmpdir):
     settings_file = tmpdir / "model" / "settings.toml"
     settings = load(settings_file)
     settings["outputs"] = [
-        {"quantity": "dummy", "sink": "dummy", "filename": "{path}/{Quantity}{suffix}"}
+        {
+            "quantity": "dummy_quantity",
+            "sink": "dummy",
+            "filename": "{path}/{Quantity}{suffix}",
+        }
     ]
     dump(settings, (settings_file))
 
@@ -309,10 +313,10 @@ def test_path_formatting(tmpdir):
         pass
 
     @register_output_quantity
-    def dummy(market, **kwargs):
+    def dummy_quantity(market, **kwargs):
         return xr.DataArray()
 
     mca = MCA.factory(Path(settings_file))
     assert str(mca.outputs(mca.market)[0]) == settings["outputs"][0]["filename"].format(
-        path=tmpdir / "model", Quantity="Dummy", suffix=".dummy"
+        path=tmpdir / "model", Quantity="Dummy_Quantity", suffix=".dummy"
     )
