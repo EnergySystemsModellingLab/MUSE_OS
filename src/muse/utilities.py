93ab030165de8b01d42ea59cb9c1cca6fcc586ee
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2020-09-08 14:21:23 +0100

    :bug: Merging new into retro assets could yield negative capacity
    Just a numerical noise issue.

--- src/muse/utilities.py
+++ src/muse/utilities.py
@@ -405,6 +405,7 @@ def merge_assets(
         result = (
             forgroup.groupby(dimension)
             .sum(dimension)
+            .clip(min=0)
             .pipe(multiindex_to_coords, dimension=dimension)
             .rename({"asset_level_%i" % i: coord for i, coord in enumerate(levels)})
         )
