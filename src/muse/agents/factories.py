EnergySystemsModellingLab/MUSE_OS

commit 55380c46096a391074459523c2f59c13387c01e7

:bug: regions in toml not handled correctly by trade quantities
The technodata and initial capacities were not reduced to include only
the region in the setting's top section. With this commit, the destination
region is fully removed if the settings only include one region.

src/muse/agents/factories.py
@@ -244,6 +244,11 @@ def agents_factory(
     if year is None:
         year = int(capacity.year.min())
 
+    if regions and "region" in capacity.dims:
+        capacity = capacity.sel(region=regions)
+    if regions and "dst_region" in capacity.dims:
+        capacity = capacity.sel(dst_region=regions).squeeze("dst_region", drop=True)
+
     result = []
     for param in params:
         if regions is not None and param["region"] not in regions:

src/muse/readers/toml.py
@@ -903,14 +903,18 @@ def read_technodata(
         technosettings.pop("commodities_out"),
         technosettings.pop("commodities_in"),
         commodities=commodities,
-    )
+    ).sel(region=regions)
     ins = (technologies.fixed_inputs > 0).any(("year", "region", "technology"))
     outs = (technologies.fixed_outputs > 0).any(("year", "region", "technology"))
     techcomms = technologies.commodity[ins | outs]
-    technologies = technologies.sel(commodity=techcomms, region=regions)
+    technologies = technologies.sel(commodity=techcomms)
     for name, value in technosettings.items():
         if isinstance(name, (Text, Path)):
             data = read_trade(value, drop="Unit")
+            if "region" in data.dims:
+                data = data.sel(region=regions)
+            if "dst_region" in data.dims:
+                data = data.sel(dst_region=regions).squeeze("dst_region", drop=True)
         else:
             data = value
         if isinstance(data, xr.Dataset):

commit d9eea1eb2b400a7b4bcf303fc1bc498a0d8c7055

:bug: when squeezing destination regions
Introduced in 55380c46 when trying to run models with fewer regions then
originally declared.

src/muse/agents/factories.py
@@ -247,7 +247,9 @@ def agents_factory(
     if regions and "region" in capacity.dims:
         capacity = capacity.sel(region=regions)
     if regions and "dst_region" in capacity.dims:
-        capacity = capacity.sel(dst_region=regions).squeeze("dst_region", drop=True)
+        capacity = capacity.sel(dst_region=regions)
+        if capacity.dst_region.size == 1:
+            capacity = capacity.squeeze("dst_region", drop=True)
 
     result = []
     for param in params:

src/muse/readers/toml.py
@@ -914,7 +914,9 @@ def read_technodata(
             if "region" in data.dims:
                 data = data.sel(region=regions)
             if "dst_region" in data.dims:
-                data = data.sel(dst_region=regions).squeeze("dst_region", drop=True)
+                data = data.sel(dst_region=regions)
+                if data.dst_region.size == 1:
+                    data = data.squeeze("dst_region", drop=True)
         else:
             data = value
         if isinstance(data, xr.Dataset):