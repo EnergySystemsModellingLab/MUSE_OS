f052dd9e77a675362001e73e93c15f4494d6d354
Alexander Kell <alexander@kell.es>
2021-01-07 14:47:01 +0000

    added read_technodata_timeslices function signature

---
+++ src/muse/readers/technodata_timeslices.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from typing import Hashable, List, Optional, Sequence, Text, Union, cast
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+
+def read_technodata_timeslices(filename: Union[Text, Path]) -> xr.Dataset:
+    pass
