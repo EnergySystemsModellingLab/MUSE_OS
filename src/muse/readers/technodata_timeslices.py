14cdad2648ffaa1ac0deed2746dc1f0fc2818267
Alexander Kell <alexander@kell.es>
2021-01-07 15:32:36 +0000

    added path of technodata_timeslices to toml

--- src/muse/readers/technodata_timeslices.py
+++ src/muse/readers/technodata_timeslices.py
@@ -7,4 +7,5 @@ import xarray as xr
 
 
 def read_technodata_timeslices(filename: Union[Text, Path]) -> xr.Dataset:
-    pass
+    csv = pd.read_csv(filename, float_precision="high", low_memory=False)
+    data = csv[csv.ProcessName != "Unit"]
