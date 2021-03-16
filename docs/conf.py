8098197f12b777185b4939e427f87db2caf1b09d
Alexander Kell <alexander@kell.es>
2021-03-16 19:12:07 +0000

    ignored build folders in exclude patterns

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ extensions = [
 ]
 source_suffix = {".rst": "restructuredtext", ".txt": "markdown", ".md": "markdown"}
 templates_path = ["_templates"]
-exclude_patterns = ["build", "**.ipynb_checkpoints", "**/ResidentialBracket*.txt"]
+exclude_patterns = ["build", "**.ipynb_checkpoints", "**/ResidentialBracket*.txt", "_sources/", "_build"]
 
 modindex_common_prefix = ["muse"]
 autodoc_typehints = "none"
@@ -44,8 +44,6 @@ intersphinx_mapping = {
     "pandas": ("http://pandas.pydata.org/pandas-docs/dev", None),
     "xarray": ("http://xarray.pydata.org/en/stable/", None),
 }
-
-bibtex_bibfiles = ["bibliography.bib"]
 
 # -- Options for HTML output -------------------------------------------------
 
