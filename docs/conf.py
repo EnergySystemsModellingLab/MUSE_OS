718adefdf0cfb6537c77d173b1a98965d2a4e334
Alexander Kell <alexander@kell.es>
2021-03-16 19:40:15 +0000

    added advanced-guide to be ignored in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ extensions = [
 ]
 source_suffix = {".rst": "restructuredtext", ".txt": "markdown", ".md": "markdown"}
 templates_path = ["_templates"]
-exclude_patterns = ["build", "**.ipynb_checkpoints", "**/ResidentialBracket*.txt", "_sources/*", "_build/*", "tutorial-code/*"]
+exclude_patterns = ["build", "**.ipynb_checkpoints", "**/ResidentialBracket*.txt", "_sources/*", "_build/*", "tutorial-code/*", "advanced-guide/*"]
 
 modindex_common_prefix = ["muse"]
 autodoc_typehints = "none"
