8354985de6c94976851d08ea7e456056b74d818e
Alexander Kell <alexander@kell.es>
2021-03-29 16:22:01 +0100

    started working on tests for timeslices

--- tests/test_timeslice_output.py
+++ tests/test_timeslice_output.py
@@ -1,5 +1,27 @@
 from pytest import approx, fixture, mark
 from xarray import DataArray
+from toml import load
+
+
+@mark.parametrize("utilization_factors", [""])
+def test_fullsim_timeslices(utilization_factors, tmpdir, compare_dirs):
+    from muse.examples import example_data_dir
+    from muse import examples
+    from muse.mca import MCA
+
+    model_path = examples.copy_model(overwrite=True)
+    settings = load(model_path / "settings.toml")
+
+    print("printing {path}")
+    # technodata_timeslices = {
+    #     "technodata_timeslices": "{path}/technodata/power/TechnodataTimeslices.csv"
+    # }
+
+    # settings["sectors"]["power"].append(technodata_timeslices)
+
+    # with tmpdir.as_cwd():
+    # MCA.factory(model_path / "settings.toml").run()
+
 
 # TODO: Unit test of one sector
 
