d4c1d440649d66b8568ca71f1c3a44e346581ca6
Alexander Kell <alexander@kell.es>
2021-03-29 14:28:04 +0100

    added test_timeslice_output with TODOs of tests

---
+++ tests/test_timeslice_output.py
@@ -0,0 +1,12 @@
+from pytest import approx, fixture, mark
+from xarray import DataArray
+
+# TODO: Unit test of one sector
+
+# TODO: Check that there is zero supply in the timeslice that the technology has zero utilization factor
+
+# TODO: Check that there is no consumption in the timeslice that the technology has zero utilization factor
+
+# TODO: Observe that there are differences in investments for technologies with low utilization factor
+
+# TODO: Check that LCOE is infinite for technology with 0 utilization factor in all timeslices
