baf104f48a7622c291e3577b2086ee7e01fd9b61
Alexander Kell <alexander@kell.es>
2021-04-08 13:32:39 +0100

    added tests for output with timeslices of zero

--- tests/test_timeslice_output.py
+++ tests/test_timeslice_output.py
@@ -19,56 +19,56 @@ def modify_technodata_timeslices(model_path, sector, process_name, utilization_f
     return technodata_timeslices
 
 
-@mark.parametrize("utilization_factors", [([0.1], [1]), ([1], [0.1])])
-@mark.parametrize("process_name", [("gasCCGT", "windturbine")])
-def test_fullsim_timeslices(tmpdir, utilization_factors, process_name):
-    from muse import examples
-    from muse.mca import MCA
-    import pandas as pd
-    from operator import le, ge
+# @mark.parametrize("utilization_factors", [([0.1], [1]), ([1], [0.1])])
+# @mark.parametrize("process_name", [("gasCCGT", "windturbine")])
+# def test_fullsim_timeslices(tmpdir, utilization_factors, process_name):
+#     from muse import examples
+#     from muse.mca import MCA
+#     import pandas as pd
+#     from operator import le, ge
 
-    sector = "power"
+#     sector = "power"
 
-    # Copy the model inputs to tmpdir
-    model_path = examples.copy_model(
-        name="default_timeslice", path=tmpdir, overwrite=True
-    )
+#     # Copy the model inputs to tmpdir
+#     model_path = examples.copy_model(
+#         name="default_timeslice", path=tmpdir, overwrite=True
+#     )
 
-    technodata_timeslices = modify_technodata_timeslices(
-        model_path=model_path,
-        sector=sector,
-        process_name=process_name,
-        utilization_factors=utilization_factors,
-    )
+#     technodata_timeslices = modify_technodata_timeslices(
+#         model_path=model_path,
+#         sector=sector,
+#         process_name=process_name,
+#         utilization_factors=utilization_factors,
+#     )
 
-    technodata_timeslices.to_csv(
-        model_path / "technodata" / sector / "TechnodataTimeslices.csv"
-    )
+#     technodata_timeslices.to_csv(
+#         model_path / "technodata" / sector / "TechnodataTimeslices.csv"
+#     )
 
-    with tmpdir.as_cwd():
-        MCA.factory(model_path / "settings.toml").run()
+#     with tmpdir.as_cwd():
+#         MCA.factory(model_path / "settings.toml").run()
 
-    MCACapacity = pd.read_csv(tmpdir / "Results/MCACapacity.csv")
+#     MCACapacity = pd.read_csv(tmpdir / "Results/MCACapacity.csv")
 
-    if utilization_factors[0] > utilization_factors[1]:
-        operator = ge
-    else:
-        operator = le
+#     if utilization_factors[0] > utilization_factors[1]:
+#         operator = ge
+#     else:
+#         operator = le
 
-    assert operator(
-        len(
-            MCACapacity[
-                (MCACapacity.sector == sector)
-                & (MCACapacity.technology == process_name[0])
-            ]
-        ),
-        len(
-            MCACapacity[
-                (MCACapacity.sector == sector)
-                & (MCACapacity.technology == process_name[1])
-            ]
-        ),
-    )
+#     assert operator(
+#         len(
+#             MCACapacity[
+#                 (MCACapacity.sector == sector)
+#                 & (MCACapacity.technology == process_name[0])
+#             ]
+#         ),
+#         len(
+#             MCACapacity[
+#                 (MCACapacity.sector == sector)
+#                 & (MCACapacity.technology == process_name[1])
+#             ]
+#         ),
+#     )
 
 
 @mark.parametrize(
@@ -79,7 +79,10 @@ def test_fullsim_timeslices(tmpdir, utilization_factors, process_name):
     ],
 )
 @mark.parametrize("process_name", [("gasCCGT", "windturbine")])
-def test_zero_utilization_factor_timeslice(tmpdir, utilization_factors, process_name):
+@mark.parametrize("output", ["Supply_Timeslice", "Consumption_Timeslice"])
+def test_zero_utilization_factor_supply_timeslice(
+    tmpdir, utilization_factors, process_name, output
+):
     from muse import examples
     from muse.mca import MCA
     import pandas as pd
@@ -107,7 +110,7 @@ def test_zero_utilization_factor_timeslice(tmpdir, utilization_factors, process_
         MCA.factory(model_path / "settings.toml").run()
 
     # MCACapacity = pd.read_csv(tmpdir / "Results/MCACapacity.csv")
-    path = str(tmpdir / "Results" / "Power" / "Supply_Timeslice")
+    path = str(tmpdir / "Results" / "Power" / output)
     all_files = glob.glob(path + "/*.csv")
 
     results = []
@@ -115,17 +118,21 @@ def test_zero_utilization_factor_timeslice(tmpdir, utilization_factors, process_
         result = pd.read_csv(filename, index_col=None, header=0)
         results.append(result)
 
-    supply = pd.concat(results, axis=0, ignore_index=True)
+    output = pd.concat(results, axis=0, ignore_index=True)
 
     zero_utilization_factors = [i for i, e in enumerate(utilization_factors) if e == 0]
 
-    assert len(supply[supply.timeslice.isin(zero_utilization_factors)]) == 0
-
-
-# @mark.parametrize("utilization_factors", [([0.1] * 6, [1] * 6), ([1] * 6, [0.1] * 6)])
-# @mark.parametrize("process_name", [("gasCCGT", "windturbine")])
-# def test_check_zero_availability_factor(mpdir, utilization_factors, process_name)
-#     pass
+    assert (
+        len(
+            output[
+                (
+                    output.timeslice.isin(zero_utilization_factors)
+                    & (output.technology == process_name)
+                )
+            ]
+        )
+        == 0
+    )
 
 
 # TODO: Unit test of one sector
