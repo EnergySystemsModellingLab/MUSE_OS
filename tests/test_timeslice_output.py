6d065c1676a979d25a14761fadae0f5687543330
Alexander Kell <alexander@kell.es>
2021-04-08 13:12:31 +0100

    added passing test for check for zero timeslice supply

--- tests/test_timeslice_output.py
+++ tests/test_timeslice_output.py
@@ -1,7 +1,7 @@
 from pytest import mark
 
 
-def modify_technodata_timeslices(model_path, sector, process_name, utilization_factor):
+def modify_technodata_timeslices(model_path, sector, process_name, utilization_factors):
     import pandas as pd
 
     technodata_timeslices = pd.read_csv(
@@ -10,18 +10,18 @@ def modify_technodata_timeslices(model_path, sector, process_name, utilization_f
 
     technodata_timeslices.loc[
         technodata_timeslices["ProcessName"] == process_name[0], "UtilizationFactor"
-    ] = utilization_factor[0]
+    ] = utilization_factors[0]
 
     technodata_timeslices.loc[
         technodata_timeslices["ProcessName"] == process_name[1], "UtilizationFactor"
-    ] = utilization_factor[1]
+    ] = utilization_factors[1]
 
     return technodata_timeslices
 
 
-@mark.parametrize("utilization_factor", [(0.1, 1), (1, 0.1)])
+@mark.parametrize("utilization_factors", [([0.1], [1]), ([1], [0.1])])
 @mark.parametrize("process_name", [("gasCCGT", "windturbine")])
-def test_fullsim_timeslices(tmpdir, utilization_factor, process_name):
+def test_fullsim_timeslices(tmpdir, utilization_factors, process_name):
     from muse import examples
     from muse.mca import MCA
     import pandas as pd
@@ -38,7 +38,7 @@ def test_fullsim_timeslices(tmpdir, utilization_factor, process_name):
         model_path=model_path,
         sector=sector,
         process_name=process_name,
-        utilization_factor=utilization_factor,
+        utilization_factors=utilization_factors,
     )
 
     technodata_timeslices.to_csv(
@@ -50,7 +50,7 @@ def test_fullsim_timeslices(tmpdir, utilization_factor, process_name):
 
     MCACapacity = pd.read_csv(tmpdir / "Results/MCACapacity.csv")
 
-    if utilization_factor[0] > utilization_factor[1]:
+    if utilization_factors[0] > utilization_factors[1]:
         operator = ge
     else:
         operator = le
@@ -69,6 +69,63 @@ def test_fullsim_timeslices(tmpdir, utilization_factor, process_name):
             ]
         ),
     )
+
+
+@mark.parametrize(
+    "utilization_factors",
+    [
+        ([0, 0, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]),
+        ([1, 1, 1, 1, 1, 1], [1, 1, 0, 0, 1, 1]),
+    ],
+)
+@mark.parametrize("process_name", [("gasCCGT", "windturbine")])
+def test_zero_utilization_factor_timeslice(tmpdir, utilization_factors, process_name):
+    from muse import examples
+    from muse.mca import MCA
+    import pandas as pd
+    import glob
+
+    sector = "power"
+
+    # Copy the model inputs to tmpdir
+    model_path = examples.copy_model(
+        name="default_timeslice", path=tmpdir, overwrite=True
+    )
+
+    technodata_timeslices = modify_technodata_timeslices(
+        model_path=model_path,
+        sector=sector,
+        process_name=process_name,
+        utilization_factors=utilization_factors,
+    )
+
+    technodata_timeslices.to_csv(
+        model_path / "technodata" / sector / "TechnodataTimeslices.csv"
+    )
+
+    with tmpdir.as_cwd():
+        MCA.factory(model_path / "settings.toml").run()
+
+    # MCACapacity = pd.read_csv(tmpdir / "Results/MCACapacity.csv")
+    path = str(tmpdir / "Results" / "Power" / "Supply_Timeslice")
+    all_files = glob.glob(path + "/*.csv")
+
+    results = []
+    for filename in all_files:
+        result = pd.read_csv(filename, index_col=None, header=0)
+        results.append(result)
+
+    supply = pd.concat(results, axis=0, ignore_index=True)
+
+    zero_utilization_factors = [i for i, e in enumerate(utilization_factors) if e == 0]
+
+    assert len(supply[supply.timeslice.isin(zero_utilization_factors)]) == 0
+
+
+# @mark.parametrize("utilization_factors", [([0.1] * 6, [1] * 6), ([1] * 6, [0.1] * 6)])
+# @mark.parametrize("process_name", [("gasCCGT", "windturbine")])
+# def test_check_zero_availability_factor(mpdir, utilization_factors, process_name)
+#     pass
 
 
 # TODO: Unit test of one sector
