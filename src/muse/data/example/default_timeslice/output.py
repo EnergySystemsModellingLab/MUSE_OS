54344f0aba76da33b6f166bb3e0607c16b7ea3b7
Alexander Kell <alexander@kell.es>
2021-04-08 13:32:10 +0100

    added tests for output with timeslices of zero

--- src/muse/data/example/default_timeslice/output.py
+++ src/muse/data/example/default_timeslice/output.py
@@ -25,3 +25,24 @@ def supply_timeslice(
         .round(rounding)
     )
     return result[result.supply != 0]
+
+
+@register_output_quantity
+def consumption_timeslice(
+    market: xr.Dataset,
+    capacity: xr.DataArray,
+    technologies: xr.Dataset,
+    sum_over: Optional[List[Text]] = None,
+    drop: Optional[List[Text]] = None,
+    rounding: int = 4,
+) -> xr.DataArray:
+    """Current consumption."""
+    print(market)
+    market = market.reset_index("timeslice")
+    result = (
+        market_quantity(market.consumption, sum_over=sum_over, drop=drop)
+        .rename("consumption")
+        .to_dataframe()
+        .round(rounding)
+    )
+    return result[result.consumption != 0]
