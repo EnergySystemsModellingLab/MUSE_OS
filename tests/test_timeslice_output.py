fc215305c4f0cdcfafa277b6afe141269c49e5aa
Giarola <sg2410@ic.ac.uk>
2021-04-29 17:15:36 +0100

    Resolved conflicts-separated min service tests

--- tests/test_timeslice_output.py
+++ tests/test_timeslice_output.py
@@ -1,4 +1,4 @@
-from pytest import mark
+from pytest import mark, raises
 
 
 def modify_technodata_timeslices(model_path, sector, process_name, utilization_factors):
@@ -15,13 +15,11 @@ def modify_technodata_timeslices(model_path, sector, process_name, utilization_f
     technodata_timeslices.loc[
         technodata_timeslices["ProcessName"] == process_name[1], "UtilizationFactor"
     ] = utilization_factors[1]
-
     technodata_timeslices["MinimumServiceFactor"] = 0
-
     return technodata_timeslices
 
 
-@mark.parametrize("utilization_factors", [([0.1], [1]), ([1], [0.1]), ([0.001], [1])])
+@mark.parametrize("utilization_factors", [([0.1], [1]), ([1], [0.1])])
 @mark.parametrize("process_name", [("gasCCGT", "windturbine")])
 def test_fullsim_timeslices(tmpdir, utilization_factors, process_name):
     from muse import examples
@@ -35,7 +33,6 @@ def test_fullsim_timeslices(tmpdir, utilization_factors, process_name):
     model_path = examples.copy_model(
         name="default_timeslice", path=tmpdir, overwrite=True
     )
-
     technodata_timeslices = modify_technodata_timeslices(
         model_path=model_path,
         sector=sector,
@@ -44,7 +41,7 @@ def test_fullsim_timeslices(tmpdir, utilization_factors, process_name):
     )
 
     technodata_timeslices.to_csv(
-        model_path / "technodata" / sector / "TechnodataTimeslices.csv"
+        model_path / "technodata" / sector / "TechnodataTimeslices.csv", index=False
     )
 
     with tmpdir.as_cwd():
@@ -105,7 +102,7 @@ def test_zero_utilization_factor_supply_timeslice(
     )
 
     technodata_timeslices.to_csv(
-        model_path / "technodata" / sector / "TechnodataTimeslices.csv"
+        model_path / "technodata" / sector / "TechnodataTimeslices.csv", index=False
     )
 
     with tmpdir.as_cwd():
@@ -136,35 +133,11 @@ def test_zero_utilization_factor_supply_timeslice(
     )
 
 
-def modify_minimum_service_factors(
-    model_path, sector, process_name, minimum_service_factor
-):
-    import pandas as pd
-
-    technodata_timeslices = pd.read_csv(
-        model_path / "technodata" / sector / "TechnodataTimeslices.csv"
-    )
-
-    technodata_timeslices.loc[
-        technodata_timeslices["ProcessName"] == process_name[0], "MinimumServiceFactor"
-    ] = minimum_service_factor[0]
-
-    technodata_timeslices.loc[
-        technodata_timeslices["ProcessName"] == process_name[1], "MinimumServiceFactor"
-    ] = minimum_service_factor[1]
-
-    return technodata_timeslices
-
-
+@mark.parametrize("utilization_factors", [([0], [1]), ([1], [0])])
 @mark.parametrize("process_name", [("gasCCGT", "windturbine")])
-@mark.parametrize(
-    "minimum_service_factor", [([1, 2, 3, 4, 5, 6], [0] * 6), ([0], [1, 2, 3, 4, 5, 6])]
-)
-def test_minimum_service_factor(tmpdir, minimum_service_factor, process_name):
+def test_all_zero_fatal_error(tmpdir, utilization_factors, process_name):
     from muse import examples
     from muse.mca import MCA
-    import pandas as pd
-    import glob
 
     sector = "power"
 
@@ -172,39 +145,16 @@ def test_minimum_service_factor(tmpdir, minimum_service_factor, process_name):
     model_path = examples.copy_model(
         name="default_timeslice", path=tmpdir, overwrite=True
     )
-
-    technodata_timeslices = modify_minimum_service_factors(
+    technodata_timeslices = modify_technodata_timeslices(
         model_path=model_path,
         sector=sector,
         process_name=process_name,
-        minimum_service_factor=minimum_service_factor,
+        utilization_factors=utilization_factors,
     )
 
     technodata_timeslices.to_csv(
-        model_path / "technodata" / sector / "TechnodataTimeslices.csv"
+        model_path / "technodata" / sector / "TechnodataTimeslices.csv", index=False
     )
 
-    with tmpdir.as_cwd():
+    with tmpdir.as_cwd(), raises(ValueError):
         MCA.factory(model_path / "settings.toml").run()
-
-    path = str(tmpdir / "Results" / "Power" / "Supply_Timeslice")
-    all_files = glob.glob(path + "/*.csv")
-
-    results = []
-    for filename in all_files:
-        result = pd.read_csv(filename, index_col=None, header=0)
-        results.append(result)
-
-    supply_timeslice = pd.concat(results)
-    supply_timeslice
-
-    for process, service_factor in zip(process_name, minimum_service_factor):
-        for i, factor in enumerate(service_factor):
-            assert (
-                supply_timeslice[
-                    (supply_timeslice.technology == process)
-                    & (supply_timeslice.commodity == "electricity")
-                    & (supply_timeslice.timeslice == i)
-                ].supply
-                >= factor
-            ).all()
