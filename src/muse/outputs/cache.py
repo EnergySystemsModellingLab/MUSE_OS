9262d8a9db3be2e27b10cf57f45f1eefc425f656
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-02-14 12:11:33 +0000

    Add OutputCache class and related magnitudes

--- src/muse/outputs/cache.py
+++ src/muse/outputs/cache.py
@@ -21,25 +21,78 @@ aggregation is quantity specific.
 """
 from __future__ import annotations
 
-from typing import List, Mapping, Text
+from typing import List, Mapping, Text, Union, Callable, Optional, MutableMapping
 
 from pubsub import pub
 import xarray as xr
+import pandas as pd
+
+from muse.registration import registrator
+
+
+OUTPUT_QUANTITY_SIGNATURE = Callable[
+    [List[xr.DataArray]], Union[xr.DataArray, pd.DataFrame]
+]
+"""Signature of functions computing quantities for later analysis."""
+
+OUTPUT_QUANTITIES: MutableMapping[Text, OUTPUT_QUANTITY_SIGNATURE] = {}
+"""Quantity for post-simulation analysis."""
+
+
+@registrator(registry=OUTPUT_QUANTITIES)
+def register_output_quantity(
+    function: Optional[OUTPUT_QUANTITY_SIGNATURE] = None,
+) -> Callable:
+    """Registers a function to compute an output quantity."""
+    from functools import wraps
+
+    @wraps(function)
+    def decorated(*args, **kwargs):
+        result = function(*args, **kwargs)
+        if isinstance(result, (pd.DataFrame, xr.DataArray)):
+            result.name = function.__name__
+        return result
+
+    return decorated
 
 
 class OutputCache:
-    def __init__(self, parameters: Mapping) -> None:
-        self.param = parameters
+    """Creates outputs functions for post-mortem analysis of cached quantities.
+
+    Each parameter is a dictionary containing the following:
+
+    - quantity (mandatory): name of the quantity to output. Mandatory.
+    - sink (optional): name of the storage procedure, e.g. the file format
+      or database format. When it cannot be guessed from `filename`, it defaults to
+      "csv".
+    - filename (optional): path to a directory or a file where to store the quantity. In
+      the latter case, if sink is not given, it will be determined from the file
+      extension. The filename can incorporate markers. By default, it is
+      "{default_output_dir}/{sector}{year}{quantity}{suffix}".
+    - any other parameter relevant to the sink, e.g. `pandas.to_csv` keyword
+      arguments.
+
+    For simplicity, it is also possible to given lone strings as input.
+    They default to `{'quantity': string}` (and the sink will default to
+    "csv").
+    """
+
+    def __init__(self, *parameters: Mapping):
+        from muse.outputs.sector import _factory
+
         self.to_save: Mapping[str, List[xr.DataArray]] = {
-            p["quantity"]: [] for p in self.param
+            p["quantity"]: [] for p in parameters if p["quantity"] in OUTPUT_QUANTITIES
         }
+        self.factory: List[Callable] = [
+            _factory(OUTPUT_QUANTITIES, p, sector_name="Cache") for p in self.to_save
+        ]
         pub.subscribe(self.cache, "cache_quantity")
 
     def cache(self, quantity: Text, data: xr.DataArray) -> None:
         """Caches the data into memory for the given quantity.
 
         Args:
-            quantity (str): The quantity this data relates to.
+            quantity (Text): The quantity this data relates to.
             data (xr.DataArray): The data to be cache.
         """
         if quantity not in self.to_save:
@@ -51,8 +104,25 @@ class OutputCache:
 
         This method is meant to be called after each time period in the main loop of the
         MCA, at the same time that market quantities are saved.
-
-        TODO: Actually implement the "save" part, ideally re-using most of the
-        infrastructure for saving stuff already in place.
         """
-        self.to_save = {p["quantity"]: [] for p in self.param}
+        for quantity, cache in self.to_save.items():
+            self.factory[quantity](cache)
+        self.to_save = {p["quantity"]: [] for p in self.to_save}
+
+
+@register_output_quantity
+def capacity(cached: List[xr.DataArray]) -> xr.DataArray:
+    """Consolidates the cached capacities into a single DataArray to save."""
+    pass
+
+
+@register_output_quantity
+def production(cached: List[xr.DataArray]) -> xr.DataArray:
+    """Consolidates the cached production into a single DataArray to save."""
+    pass
+
+
+@register_output_quantity
+def lcoe(cached: List[xr.DataArray]) -> xr.DataArray:
+    """Consolidates the cached LCOE into a single DataArray to save."""
+    pass
