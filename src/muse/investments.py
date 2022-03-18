214d519f18b5f2d888bc5975c045e29a83ef5fb3
Giarola <sg2410@ic.ac.uk>
2022-03-18 22:18:35 +0000

    Sort packages in investment

--- src/muse/investments.py
+++ src/muse/investments.py
@@ -240,8 +240,8 @@ def adhoc_match_demand(
     timeslice_op: Optional[Callable[[xr.DataArray], xr.DataArray]] = None,
 ) -> xr.DataArray:
     from muse.demand_matching import demand_matching
-    from muse.quantities import maximum_production, capacity_in_use
-    from muse.timeslices import convert_timeslice, QuantityType
+    from muse.quantities import capacity_in_use, maximum_production
+    from muse.timeslices import QuantityType, convert_timeslice
 
     demand = next((c for c in constraints if c.name == "demand")).b
 
@@ -295,9 +295,9 @@ def scipy_match_demand(
     timeslice_op: Optional[Callable[[xr.DataArray], xr.DataArray]] = None,
     **options,
 ) -> xr.DataArray:
-    from muse.constraints import ScipyAdapter
-    from scipy.optimize import linprog
     from logging import getLogger
+    from scipy.optimize import linprog
+    from muse.constraints import ScipyAdapter
 
     if "timeslice" in costs.dims and timeslice_op is not None:
         costs = timeslice_op(costs)
@@ -330,8 +330,8 @@ def cvxopt_match_demand(
     timeslice_op: Optional[Callable[[xr.DataArray], xr.DataArray]] = None,
     **options,
 ) -> xr.DataArray:
-    from logging import getLogger
     from importlib import import_module
+    from logging import getLogger
     from muse.constraints import ScipyAdapter
 
     if "year" in technologies.dims and year is None:
