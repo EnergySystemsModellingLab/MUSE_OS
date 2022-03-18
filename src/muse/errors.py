9a5f3aa71df479c3588ac49a448440a132aacf81
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-03-18 13:26:12 +0000

    Add UnitsConflictInCommodities exception

--- src/muse/errors.py
+++ src/muse/errors.py
@@ -2,8 +2,19 @@ class RetrofitAgentNotDefined(Exception):
     """Indicates that the retrofit agent has not been defined."""
 
     msg = """The retrofit agent has not been defined. This might be because it actually
-has not been defined in the agents file or because there is a typo in its name in either
-the agents and or technodata files."""
+has not been defined in the agents file or because it cannot be found because there is a
+typo in its name in either the agents and or technodata files."""
+
+    def __str__(self):
+        return self.msg
+
+
+class UnitsConflictInCommodities(Exception):
+    """Indictaes that there is a conflcit in the commodity units between files."""
+
+    msg = """The units of “CommIn” “CommOut” and “GlobalCommodities” files must be the
+same, including the casing. Check the consistency of the units across those thre files.
+"""
 
     def __str__(self):
         return self.msg
