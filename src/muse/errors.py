d8a2111da5da11d18bc96022a4cc99faf6d3b984
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-03-23 09:15:21 +0000

    Amend docstring and error messages

--- src/muse/errors.py
+++ src/muse/errors.py
@@ -2,15 +2,19 @@ class RetrofitAgentNotDefined(Exception):
     """Indicates that the retrofit agent has not been defined."""
 
     msg = """The retrofit agent has not been defined. This might be because it actually
-has not been defined in the agents file or because it cannot be found because there is a
-typo in its name in either the agents and or technodata files."""
+has not been defined in the agents file or specified in the technodata file or it cannot
+be found because there is a typo in its name in either the agents and or technodata
+files. When a retrofit agent is defined in the agent file, the same name should be
+reported as an additional column in the technodata, assigning the fraction of the
+initial stock assigned to each retrofit agent. In presence of multiple retrofit agents,
+a new column needs to be added per agent."""
 
     def __str__(self):
         return self.msg
 
 
 class UnitsConflictInCommodities(Exception):
-    """Indictaes that there is a conflcit in the commodity units between files."""
+    """Indicates that there is a conflcit in the commodity units between files."""
 
     msg = """The units of “CommIn” “CommOut” and “GlobalCommodities” files must be the
 same, including the casing. Check the consistency of the units across those thre files.
@@ -21,7 +25,7 @@ same, including the casing. Check the consistency of the units across those thre
 
 
 class GrowthOfCapacityTooConstrained(Exception):
-    """Indicataes that the investment step failed because capacity could not grow."""
+    """Indicates that the investment step failed because capacity could not grow."""
 
     msg = """Error during the investment process. The capacity was not allowed to grow
 suficiently in order to match the demand. Consider increating the MaxCapacityAddition
