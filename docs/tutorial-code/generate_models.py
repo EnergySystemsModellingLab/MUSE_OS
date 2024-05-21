935940ad80a82430cb9bf2491aa095862c3a061b
Tom Bland <tom_bland@hotmail.co.uk>
2024-05-21 11:42:24 +0100

    Sorting paths for generating models

--- docs/tutorial-code/generate_models.py
+++ docs/tutorial-code/generate_models.py
@@ -3,6 +3,6 @@ import sys
 from pathlib import Path
 
 if __name__ == "__main__":
-    for tutorial in Path(__file__).parent.iterdir():
+    for tutorial in sorted(Path(__file__).parent.iterdir()):
         if (tutorial / "generate_models.py").exists():
             subprocess.call([sys.executable, str(tutorial / "generate_models.py")])
