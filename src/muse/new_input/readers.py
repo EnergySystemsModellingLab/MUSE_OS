71d6388cab4c886140397a5608c1940930a41c7b
Tom Bland <tom_bland@hotmail.co.uk>
2024-07-04 09:29:16 +0100

    Update tables for new csv columns

--- src/muse/new_input/readers.py
+++ src/muse/new_input/readers.py
@@ -31,21 +31,21 @@ def read_inputs(data_dir):
 
 def read_commodities_csv(buffer_, con):
     sql = """CREATE TABLE commodities (
-      name VARCHAR PRIMARY KEY,
+      id VARCHAR PRIMARY KEY,
       type VARCHAR CHECK (type IN ('energy', 'service', 'material', 'environmental')),
       unit VARCHAR,
     );
     """
     con.sql(sql)
     rel = con.read_csv(buffer_, header=True, delimiter=",")  # noqa: F841
-    con.sql("INSERT INTO commodities SELECT name, type, unit FROM rel;")
-    return con.sql("select name, type, unit from commodities").fetchnumpy()
+    con.sql("INSERT INTO commodities SELECT id, type, unit FROM rel;")
+    return con.sql("select * from commodities").fetchnumpy()
 
 
 def read_commodity_trade_csv(buffer_, con):
     sql = """CREATE TABLE commodity_trade (
-    commodity VARCHAR REFERENCES commodities(name),
-    region VARCHAR REFERENCES regions(name),
+    commodity VARCHAR REFERENCES commodities(id),
+    region VARCHAR REFERENCES regions(id),
     year BIGINT,
     import DOUBLE,
     export DOUBLE,
@@ -54,68 +54,68 @@ def read_commodity_trade_csv(buffer_, con):
     con.sql(sql)
     rel = con.read_csv(buffer_, header=True, delimiter=",")  # noqa: F841
     con.sql("""INSERT INTO commodity_trade SELECT
-            commodity, region, year, import, export FROM rel;""")
+            commodity_id, region_id, year, import, export FROM rel;""")
     return con.sql("SELECT * from commodity_trade").fetchnumpy()
 
 
 def read_commodity_costs_csv(buffer_, con):
     sql = """CREATE TABLE commodity_costs (
+    commodity VARCHAR REFERENCES commodities(id),
+    region VARCHAR REFERENCES regions(id),
     year BIGINT,
-    region VARCHAR REFERENCES regions(name),
-    commodity VARCHAR REFERENCES commodities(name),
     value DOUBLE,
     );
     """
     con.sql(sql)
     rel = con.read_csv(buffer_, header=True, delimiter=",")  # noqa: F841
     con.sql("""INSERT INTO commodity_costs SELECT
-            year, region, commodity_name, value FROM rel;""")
+            commidity_id, region_id, year, value FROM rel;""")
     return con.sql("SELECT * from commodity_costs").fetchnumpy()
 
 
 def read_demand_csv(buffer_, con):
     sql = """CREATE TABLE demand (
+    commodity VARCHAR REFERENCES commodities(id),
+    region VARCHAR REFERENCES regions(id),
     year BIGINT,
-    commodity VARCHAR REFERENCES commodities(name),
-    region VARCHAR REFERENCES regions(name),
     demand DOUBLE,
     );
     """
     con.sql(sql)
     rel = con.read_csv(buffer_, header=True, delimiter=",")  # noqa: F841
-    con.sql("INSERT INTO demand SELECT year, commodity_name, region, demand FROM rel;")
+    con.sql("INSERT INTO demand SELECT commodity_id, region_id, year, demand FROM rel;")
     return con.sql("SELECT * from demand").fetchnumpy()
 
 
 def read_demand_slicing_csv(buffer_, con):
     sql = """CREATE TABLE demand_slicing (
-    commodity VARCHAR REFERENCES commodities(name),
-    region VARCHAR REFERENCES regions(name),
+    commodity VARCHAR REFERENCES commodities(id),
+    region VARCHAR REFERENCES regions(id),
+    year BIGINT,
     timeslice VARCHAR,
     fraction DOUBLE CHECK (fraction >= 0 AND fraction <= 1),
-    year BIGINT,
     );
     """
     con.sql(sql)
     rel = con.read_csv(buffer_, header=True, delimiter=",")  # noqa: F841
     con.sql("""INSERT INTO demand_slicing SELECT
-            commodity, region, timeslice, fraction, year FROM rel;""")
+            commodity_id, region_id, year, timeslice, fraction FROM rel;""")
     return con.sql("SELECT * from demand_slicing").fetchnumpy()
 
 
 def read_regions_csv(buffer_, con):
     sql = """CREATE TABLE regions (
-      name VARCHAR PRIMARY KEY,
+      id VARCHAR PRIMARY KEY,
     );
     """
     con.sql(sql)
     rel = con.read_csv(buffer_, header=True, delimiter=",")  # noqa: F841
-    con.sql("INSERT INTO regions SELECT name FROM rel;")
-    return con.sql("SELECT name from regions").fetchnumpy()
+    con.sql("INSERT INTO regions SELECT id FROM rel;")
+    return con.sql("SELECT * from regions").fetchnumpy()
 
 
 def calculate_global_commodities(commodities):
-    names = commodities["name"].astype(np.dtype("str"))
+    names = commodities["id"].astype(np.dtype("str"))
     types = commodities["type"].astype(np.dtype("str"))
     units = commodities["unit"].astype(np.dtype("str"))
 
