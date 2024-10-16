a61a036f8405b58a9df2039a79b4f875ac1019ad
Tom Bland <tom_bland@hotmail.co.uk>
2024-10-16 12:05:20 +0100

    Fix max_production function

--- src/muse/quantities.py
+++ src/muse/quantities.py
@@ -344,6 +344,7 @@ def maximum_production(technologies: xr.Dataset, capacity: xr.DataArray, **filte
         filters and the set of technologies in `capacity`.
     """
     from muse.commodities import is_enduse
+    from muse.timeslices import TIMESLICE, QuantityType, convert_timeslice
     from muse.utilities import broadcast_techs, filter_input
 
     capa = filter_input(
@@ -355,7 +356,11 @@ def maximum_production(technologies: xr.Dataset, capacity: xr.DataArray, **filte
     ftechs = filter_input(
         btechs, **{k: v for k, v in filters.items() if k in btechs.dims}
     )
-    result = capa * ftechs.fixed_outputs * ftechs.utilization_factor
+    result = (
+        capa
+        * convert_timeslice(ftechs.fixed_outputs, TIMESLICE, QuantityType.EXTENSIVE)
+        * ftechs.utilization_factor
+    )
     return result.where(is_enduse(result.comm_usage), 0)
 
 
@@ -457,7 +462,6 @@ def costed_production(
     service is applied first.
     """
     from muse.quantities import maximum_production
-    from muse.timeslices import QuantityType, convert_timeslice
     from muse.utilities import broadcast_techs
 
     technodata = cast(xr.Dataset, broadcast_techs(technologies, capacity))
@@ -473,11 +477,7 @@ def costed_production(
             return xr.Dataset(dict(x=x)).groupby("region").sum("asset").x
 
     ranking = costs.rank("asset")
-    maxprod = convert_timeslice(
-        maximum_production(technodata, capacity),
-        demand.timeslice,
-        QuantityType.EXTENSIVE,
-    )
+    maxprod = maximum_production(technodata, capacity)
     commodity = (maxprod > 0).any([i for i in maxprod.dims if i != "commodity"])
     commodity = commodity.drop_vars(
         [u for u in commodity.coords if u not in commodity.dims]
