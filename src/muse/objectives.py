425ee98d9e1c9fdb043c3b8abf7eff11913bbcff
Tom Bland <tom_bland@hotmail.co.uk>
2024-11-28 16:19:47 +0000

    Add checks for demand_share dimensions

--- src/muse/objectives.py
+++ src/muse/objectives.py
@@ -165,7 +165,9 @@ def register_objective(function: OBJECTIVE_SIGNATURE):
         from logging import getLogger
 
         # Check inputs
-        check_dimensions(demand, ["asset", "timeslice", "commodity"])
+        check_dimensions(
+            demand, ["asset", "timeslice", "commodity"], optional=["region"]
+        )
         check_dimensions(
             technologies, ["replacement", "commodity"], optional=["timeslice"]
         )
