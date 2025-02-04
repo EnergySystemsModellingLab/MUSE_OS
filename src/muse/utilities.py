490014f48e2bbaeec78f3746b3cec8e5c96567b5
Tom Bland <tom_bland@hotmail.co.uk>
2025-02-04 14:55:09 +0000

    Fix mistakes from merge

--- src/muse/utilities.py
+++ src/muse/utilities.py
@@ -201,9 +201,6 @@ def broadcast_over_assets(
     example, it could also be used on a dataset of commodity prices to select prices
     relevant to each asset (e.g. if assets exist in multiple regions).
 
-    This function does not support arrays with a 'year' dimension. Please select
-    data for a specific year before calling this function.
-
     Arguments:
         data: The dataset/data-array to broadcast
         template: The dataset/data-array to use as a template
--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -248,7 +248,6 @@ def test_yearly_aggregate_no_outputs(tmpdir):
     assert len(outputs(None, year=2010)) == 0
 
 
-@mark.xfail
 def test_mca_aggregate_outputs(tmpdir):
     from toml import dump, load
 
