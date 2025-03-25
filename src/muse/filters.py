b25a65c7047d9e550a81fe3f65c4232be11c69b8
Tom Bland <tom_bland@hotmail.co.uk>
2025-03-25 11:42:07 +0000

    Limit search space to relevant techs (experimental)

--- src/muse/filters.py
+++ src/muse/filters.py
@@ -436,12 +436,24 @@ def initialize_from_technologies(
         ("asset", demand.asset.values),
         ("replacement", technologies.technology.values),
     )
-    return xr.DataArray(
+    search_space = xr.DataArray(
         np.ones(tuple(len(u[1]) for u in coords), dtype=bool),
         coords=coords,
         dims=[u[0] for u in coords],
         name="search_space",
     )
+
+    # Only consider technologies that produce demanded commodities
+    demanded_commodities = (demand > 0).any("timeslice")
+    produces_commodity = (
+        (technologies.fixed_outputs > 0)
+        .any(dim="region")
+        .rename(technology="replacement")
+    )
+    produces_demanded_commodity = (produces_commodity * demanded_commodities).any(
+        "commodity"
+    )
+    return search_space & produces_demanded_commodity
 
 
 @register_initializer(name="from_assets")
