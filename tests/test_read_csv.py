c31c422b24f8f95c18cc5af9131491187fe3a4a5
Tom Bland <23723407+tsmbland@users.noreply.github.com>
2025-06-14 10:41:11 +0100

    Tests foor correlation model

--- tests/test_read_csv.py
+++ tests/test_read_csv.py
@@ -6,10 +6,13 @@ from muse.readers.csv import (
     read_global_commodities_csv,
     read_initial_assets_csv,
     read_initial_market_csv,
+    read_macro_drivers_csv,
     read_presets_csv,
+    read_regression_parameters_csv,
     read_technodata_timeslices_csv,
     read_technodictionary_csv,
     read_technologies_csv,
+    read_timeslice_shares_csv,
 )
 
 
@@ -24,6 +27,13 @@ def model_path(tmp_path):
 def timeslice_model_path(tmp_path):
     """Creates temporary folder containing the default model."""
     examples.copy_model(name="default_timeslice", path=tmp_path)
+    return tmp_path / "model"
+
+
+@pytest.fixture
+def correlation_model_path(tmp_path):
+    """Creates temporary folder containing the correlation model."""
+    examples.copy_model(name="default_correlation", path=tmp_path)
     return tmp_path / "model"
 
 
@@ -62,7 +72,6 @@ def test_read_technodata_timeslices_csv(timeslice_model_path):
     """Test reading the technodata timeslices CSV file."""
     timeslices_path = timeslice_model_path / "power" / "TechnodataTimeslices.csv"
     timeslices_df = read_technodata_timeslices_csv(timeslices_path)
-    assert timeslices_df is not None
     mandatory_columns = {
         "utilization_factor",
         "technology",
@@ -89,11 +98,6 @@ def test_read_technologies_csv(timeslice_model_path):
         technodata_path, comm_out_path, comm_in_path, timeslices_path
     )
 
-    assert technodata_df is not None
-    assert comm_out_df is not None
-    assert comm_in_df is not None
-    assert timeslices_df is not None
-
     # Check required columns
     for df in [technodata_df, comm_out_df, comm_in_df]:
         assert "technology" in df.columns
@@ -105,7 +109,6 @@ def test_read_initial_assets_csv(model_path):
     """Test reading the initial assets CSV file."""
     assets_path = model_path / "power" / "ExistingCapacity.csv"
     assets_df = read_initial_assets_csv(assets_path)
-    assert assets_df is not None
     mandatory_columns = {
         "region",
         "technology",
@@ -127,7 +130,6 @@ def test_read_global_commodities_csv(model_path):
     """Test reading the global commodities CSV file."""
     commodities_path = model_path / "GlobalCommodities.csv"
     commodities_df = read_global_commodities_csv(commodities_path)
-    assert commodities_df is not None
     mandatory_columns = {
         "commodity",
         "comm_type",
@@ -136,20 +138,30 @@ def test_read_global_commodities_csv(model_path):
     assert set(commodities_df.columns) == mandatory_columns | extra_columns
 
 
-# def test_read_timeslice_shares_csv(model_path):
-#     """Test reading the timeslice shares CSV file."""
-#     shares_path = model_path / "power" / "TimesliceShares.csv"
-#     shares_df = read_timeslice_shares_csv(shares_path)
-#     assert shares_df is not None
-#     assert "region" in shares_df.columns
-#     assert "timeslice" in shares_df.columns
+def test_read_timeslice_shares_csv(correlation_model_path):
+    """Test reading the timeslice shares CSV file."""
+    shares_path = (
+        correlation_model_path / "residential_presets" / "TimesliceSharepreset.csv"
+    )
+    shares_df = read_timeslice_shares_csv(shares_path)
+    mandatory_columns = {
+        "region",
+        "timeslice",
+    }
+    extra_columns = {
+        "electricity",
+        "heat",
+        "wind",
+        "gas",
+        "CO2f",
+    }
+    assert set(shares_df.columns) == mandatory_columns | extra_columns
 
 
 def test_read_agent_parameters_csv(model_path):
     """Test reading the agent parameters CSV file."""
     agents_path = model_path / "Agents.csv"
     agents_df = read_agent_parameters_csv(agents_path)
-    assert agents_df is not None
     mandatory_columns = {
         "search_rule",
         "quantity",
@@ -175,22 +187,28 @@ def test_read_agent_parameters_csv(model_path):
     assert set(agents_df.columns) == mandatory_columns | extra_columns
 
 
-# def test_read_macro_drivers_csv(model_path):
-#     """Test reading the macro drivers CSV file."""
-#     macro_path = model_path / "power" / "MacroDrivers.csv"
-#     macro_df = read_macro_drivers_csv(macro_path)
-#     assert macro_df is not None
-#     assert "region" in macro_df.columns
-#     assert "variable" in macro_df.columns
-#     assert "Population" in macro_df["variable"].values
-#     assert "GDP|PPP" in macro_df["variable"].values
+def test_read_macro_drivers_csv(correlation_model_path):
+    """Test reading the macro drivers CSV file."""
+    macro_path = correlation_model_path / "residential_presets" / "Macrodrivers.csv"
+    macro_df = read_macro_drivers_csv(macro_path)
+    mandatory_columns = {
+        "region",
+        "variable",
+    }
+    extra_columns = {
+        "unit",
+        *[str(year) for year in range(2010, 2111)],
+    }
+    assert set(macro_df.columns) == mandatory_columns | extra_columns
+
+    assert "Population" in macro_df["variable"].values
+    assert "GDP|PPP" in macro_df["variable"].values
 
 
 def test_read_initial_market_csv(model_path):
     """Test reading the initial market CSV files."""
     projections_path = model_path / "Projections.csv"
-    projections_df, import_df, export_df = read_initial_market_csv(projections_path)
-    assert projections_df is not None
+    projections_df, _, _ = read_initial_market_csv(projections_path)
     mandatory_columns = {
         "year",
         "attribute",
@@ -206,22 +224,31 @@ def test_read_initial_market_csv(model_path):
     assert set(projections_df.columns) == mandatory_columns | extra_columns
 
 
-# def test_read_regression_parameters_csv(model_path):
-#     """Test reading the regression parameters CSV file."""
-#     regression_path = model_path / "power" / "RegressionParameters.csv"
-#     regression_df = read_regression_parameters_csv(regression_path)
-#     assert regression_df is not None
-#     assert "sector" in regression_df.columns
-#     assert "region" in regression_df.columns
-#     assert "function_type" in regression_df.columns
-#     assert "coeff" in regression_df.columns
+def test_read_regression_parameters_csv(correlation_model_path):
+    """Test reading the regression parameters CSV file."""
+    regression_path = (
+        correlation_model_path / "residential_presets" / "regressionparameters.csv"
+    )
+    regression_df = read_regression_parameters_csv(regression_path)
+    mandatory_columns = {
+        "sector",
+        "region",
+        "function_type",
+        "coeff",
+    }
+    extra_columns = {
+        "CO2f",
+        "electricity",
+        "gas",
+        "heat",
+    }
+    assert set(regression_df.columns) == mandatory_columns | extra_columns
 
 
 def test_read_presets_csv(model_path):
     """Test reading the presets CSV files."""
     presets_path = model_path / "residential_presets" / "*.csv"
     presets_dict = read_presets_csv(presets_path)
-    assert presets_dict is not None
     assert len(presets_dict) > 0
 
     # Check first year's data
