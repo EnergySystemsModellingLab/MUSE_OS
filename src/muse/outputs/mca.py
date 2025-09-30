4585da1f26720615228315e92fe5a76c3ffaeb93
Tom Bland <tom_bland@hotmail.co.uk>
2025-09-30 08:35:41 +0100

    Copy market object to prevent side effects

--- src/muse/outputs/mca.py
+++ src/muse/outputs/mca.py
@@ -376,7 +376,7 @@ def sector_lcoe(
     agents = retro if len(retro) > 0 else new
     if len(technologies) > 0:
         for agent in agents:
-            agent_market = market.sel(year=agent.year)
+            agent_market = market.sel(year=agent.year).copy(deep=True)
             agent_market["consumption"] = agent_market.consumption * agent.quantity
 
             # Filter commodities based on end-use status
@@ -465,7 +465,7 @@ def sector_eac(
     agents = retro if len(retro) > 0 else new
     if len(technologies) > 0:
         for agent in agents:
-            agent_market = market.sel(year=agent.year)
+            agent_market = market.sel(year=agent.year).copy(deep=True)
             agent_market["consumption"] = agent_market.consumption * agent.quantity
 
             # Filter commodities based on end-use status
